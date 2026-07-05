d5a6af49e2a69fe37d5b6b0bfe3120ee2bb2522a
Hamidov Ali <hemidoveli893@gmail.com>
2026-07-06 02:02:48 +0400

    feat: add ingestion logic

---
+++ app/core/ingestion.py
@@ -0,0 +1,29 @@
+import os 
+import fitz 
+from config.settings import settings 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_chroma import Chroma 
+
+def ingest_document(file_name: str):
+    file_path = os.path.join("data","raw", file_name)
+    
+    if not os.path.exists(file_path):
+        return f"Error: {file_path} not found!"
+    
+    doc = fitz.open(file_path)
+    text = "".join([page.get_text() for page in doc])
+    
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = settings.CHUNK_SIZE,
+        chunk_overlap = settings.CHUNK_OVERLAP
+    )
+    chunks = splitter.split_text(text)
+    
+    vectorstore = Chroma.from_texts(
+        texts = chunks,
+        embedding = OllamaEmbeddings(model=settings.EMBEDDING_MODEL),
+        persist_directory = settings.CHROMA_PATH
+    )
+    
+    return "Successfully ingested {len(chunks)} chunks from {file_name} into vector store."
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from datascience-rag-bot!")
-
-
-if __name__ == "__main__":
-    main()
