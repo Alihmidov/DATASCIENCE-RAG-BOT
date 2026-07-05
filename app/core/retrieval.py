9b7d71015b3b1108ec3083c7c23ffd1a8f7e93d3
Hamidov Ali <hemidoveli893@gmail.com>
2026-07-06 02:03:16 +0400

    feat: add vector retrieval logic

---
+++ app/core/retrieval.py
@@ -0,0 +1,14 @@
+from config.settings import settings 
+from langchain_community.embeddings import OllamaEmbeddings 
+from langchain_chroma import Chroma 
+
+def retrieve_context(query: str):
+    vectorstore = Chroma(
+        embedding_function=OllamaEmbeddings(model=settings.EMBEDDING_MODEL),
+        persist_directory=settings.CHROMA_PATH
+    )
+    
+    res = vectorstore.similarity_search(query, k=3)
+    context = "/n/n".join([chunk.page_context for chunk in res])
+    
+    return context
