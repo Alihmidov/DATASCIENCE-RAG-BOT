120a73f82b71d9f113be9657f3897425eb620e48
Hamidov Ali <hemidoveli893@gmail.com>
2026-07-09 19:05:51 +0400

    refactor: remove unnecessary codes from test files

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,10 +2,6 @@ from fastapi.testclient import TestClient
 from app.main import app
 import pytest
 
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 client = TestClient(app)
 
 @pytest.mark.skip_if_no_ollama
--- tests/test_rag.py
+++ tests/test_rag.py
@@ -1,11 +1,6 @@
 from app.core.llm_logic import ask_bot
 import pytest
 
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
 @pytest.mark.skip_if_no_ollama
 def test_answer():
     question = "Who is Michael Jackson?"
